708f808615e2f9df983729aa48c67ba344935a5b
Rasul <rasuljonhalimov47@gmail.com>
2022-01-03 12:06:49 +0500

    finish Home, Sidebar & Navbar components

---
+++ frontend/src/components/CreatePin.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreatePin() {
+  return <div>CreatePin CreatePin</div>;
+}
+
+export default CreatePin;
---
+++ frontend/src/components/Search.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Search() {
+  return <div>Search Search;</div>;
+}
+
+export default Search;
